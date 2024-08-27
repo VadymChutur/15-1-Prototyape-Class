79582b68b4da851344ac0cfa3b4cbd7ce384ebe0
Vadym Chutur <falloutgreck@gmail.com>
2024-08-27 16:53:31 +0300

    Update script.js

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -21,23 +21,31 @@ console.log('Hello world');
 // console.log('User 1: ', user1);
 // console.log('User 2: ', user2);
 
-const user = {
-  name: 'Test User a',
-  sayHello() {
-    console.log(this.name);
-  },
-};
+// const user = {
+//   name: 'Test User a',
+//   sayHello() {
+//     console.log(this.name);
+//   },
+// };
 
-const admin = Object.create(user);
-admin.age = 19;
-admin.showAge = function () {
-  console.log(this.age);
-};
+// const admin = Object.create(user);
+// admin.age = 19;
+// admin.showAge = function () {
+//   console.log(this.age);
+// };
 
-console.log(admin);
-const practiceOwner = Object.create(admin);
-practiceOwner.name = 'Owner';
-practiceOwner.age = 35;
-console.log(practiceOwner);
-practiceOwner.sayHello();
-practiceOwner.showAge();
+// console.log(admin);
+// const practiceOwner = Object.create(admin);
+// practiceOwner.name = 'Owner';
+// practiceOwner.age = 35;
+// console.log(practiceOwner);
+// practiceOwner.sayHello();
+// practiceOwner.showAge();
+
+class User {
+  constructor() {
+    console.log(this);
+  }
+}
+
+const test = new User();
